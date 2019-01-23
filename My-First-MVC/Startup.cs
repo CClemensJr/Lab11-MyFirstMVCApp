6ed66785c87099b1b9e68ee136e3f60976703b62
Charles Clemens <cclemensjr@gmail.com>
2019-01-23 09:59:21 -0800

    Esstablished router

--- My-First-MVC/Startup.cs
+++ My-First-MVC/Startup.cs
@@ -26,11 +26,14 @@ namespace My_First_MVC
                 app.UseDeveloperExceptionPage();
             }
 
-            
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            
         }
     }
 }
