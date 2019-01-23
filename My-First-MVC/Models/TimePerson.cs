3ceb076aa2e10398ce2425f8bc1496eb05e9d2c8
Charles Clemens <cclemensjr@gmail.com>
2019-01-23 11:35:20 -0800

    created model with properties and method signature

--- My-First-MVC/Models/TimePerson.cs
+++ My-First-MVC/Models/TimePerson.cs
@@ -7,5 +7,26 @@ namespace My_First_MVC.Models
 {
     public class TimePerson
     {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
+        public static List<TimePerson> GetPersons(int startYear, int endYear)
+        {
+            // read in file
+            // iterate through the array and set the values appropriately to a new TimePerson object
+            // CSV is comma delimited
+            // Create full list of people from csv
+            // Then do LINQ wuery (with lambda expression) to filter
+            // List<timePerson>
+            // Return list
+            return null;
+        }
     }
 }
