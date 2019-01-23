a44e156f97bc06a0fa5ef9dd209e87a33385789f
Charles Clemens <cclemensjr@gmail.com>
2019-01-23 10:06:20 -0800

    Created HomeController

---
+++ My-First-MVC/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_First_MVC.Controllers
+{
+    public class HomeController
+    {
+    }
+}
