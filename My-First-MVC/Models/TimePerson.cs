d35e8b56d7a890476cc26d5251a6c1fbb2720cce
Charles Clemens <cclemensjr@gmail.com>
2019-01-24 12:54:53 -0800

    instantiated objects and added to list

--- My-First-MVC/Models/TimePerson.cs
+++ My-First-MVC/Models/TimePerson.cs
@@ -9,14 +9,14 @@ namespace My_First_MVC.Models
     public class TimePerson
     {
         public int Year { get; set; }
-        public string Honor { get; set; }
-        public string Name { get; set; }
-        public string Country { get; set; }
-        public int BirthYear { get; set; }
-        public int DeathYear { get; set; }
-        public string Title { get; set; }
-        public string Category { get; set; }
-        public string Context { get; set; }
+        public string Honor { get; set; } = "Data not provided";
+        public string Name { get; set; } = "Data not provided";
+        public string Country { get; set; } = "Data not provided";
+        public int BirthYear { get; set; } = 0;
+        public int DeathYear { get; set; } = 0;
+        public string Title { get; set; } = "Data not provided";
+        public string Category { get; set; } = "Data not provided";
+        public string Context { get; set; } = "Data not provided";
 
         public static List<TimePerson> GetPersons(int startYear, int endYear)
         {
@@ -24,28 +24,24 @@ namespace My_First_MVC.Models
 
             string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../wwwroot/personOfTheYear.csv");
             string[] personOfTheYearData = File.ReadAllLines(path);
-            //string[] personOfTheYearArray = personOfTheYearText.Split(',');
-            //TimePerson[] timePerson = new TimePerson[personOfTheYearArray.Length];
 
-            //// iterate through the array and set the values appropriately to a new TimePerson object
+            // iterate through the array and set the values appropriately to a new TimePerson object
             for (int i = 1; i < personOfTheYearData.Length; i++)
             {
-                
+                var dataString = personOfTheYearData[i].Split(',');
 
-                people.Add(new TimePerson
+                people.Add(new TimePerson()
                 {
-                    Year = 1980,
-                    Honor = personOfTheYearData[i],
-                    Name = personOfTheYearData[i],
-                    //Country = personOfTheYearData[i],
-                    //BirthYear = int.Parse(personOfTheYearData[i]),
-                    //DeathYear = int.Parse(personOfTheYearData[i]),
-                    //Title = personOfTheYearData[i],
-                    //Category = personOfTheYearData[i],
-                    //Context = personOfTheYearData[i]
+                    Year = (dataString[0] == "") ? 0 : Convert.ToInt32(dataString[0]),
+                    Honor = dataString[1],
+                    Name = dataString[2],
+                    Country = personOfTheYearData[3],
+                    BirthYear = (dataString[4] == "") ? 0 : Convert.ToInt32(dataString[4]),
+                    DeathYear = (dataString[5] == "") ? 0 : Convert.ToInt32(dataString[5]),
+                    Title = personOfTheYearData[6],
+                    Category = personOfTheYearData[7],
+                    Context = personOfTheYearData[8]
                 });
-
-                //people.Add(timePerson[i]);
             }
 
             // Create full list of people from csv
